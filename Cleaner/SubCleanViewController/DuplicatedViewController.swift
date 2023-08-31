e7340d3c67e77ba08ea4218a267f0b1126269f62
Duc Nguyen <108277804+HornSeller@users.noreply.github.com>
2023-08-31 17:13:11 +0700

    cmt

--- Cleaner/SubCleanViewController/DuplicatedViewController.swift
+++ Cleaner/SubCleanViewController/DuplicatedViewController.swift
@@ -34,108 +34,11 @@ class DuplicatedViewController: UIViewController, UITableViewDelegate, UITableVi
         tableView.rowHeight = 0.2582 * view.frame.height
         
         DispatchQueue.main.async {
-            self.fetchAllPhotos { hashArr, images in
-                self.hashArr = hashArr
-                self.images = images
-                var result: [[UIImage]] = []
-                var currentIndex = 0
-                var addedElement: [String] = []
-                
-                while currentIndex < self.hashArr.count {
-                    let currentString = self.hashArr[currentIndex]
-                    var currentGroup: [String] = [currentString]
-                    
-                    let currentImage = self.images[currentIndex]
-                    var currentImageGroup: [UIImage] = [currentImage]
-                    
-                    var nextIndex = currentIndex + 1
-                    while nextIndex < self.hashArr.count {
-                        if self.hashArr[nextIndex] == currentString && !addedElement.contains(currentString) {
-                            currentGroup.append(self.hashArr[nextIndex])
-                            currentImageGroup.append(self.images[nextIndex])
-                        }
-                        nextIndex += 1
-                    }
-                    if currentGroup.count >= 2 {
-                        result.append(currentImageGroup)
-                        addedElement.append(currentString)
-                    }
-                    
-                    currentIndex += 1
-                    if currentIndex == self.hashArr.count {
-                        self.dataTable = result
-                        self.tableView.reloadData()
-                        print(result)
-                    }
-                }
-                
-                print(self.dataTable.count)
-            }
+            self.dataTable = CleanViewController.duplicatedDataTable
         }
     }
     
     @IBAction func backBtnTapped(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
     }
-    
-    func resizeImage(image: UIImage, newSize: CGSize) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        image.draw(in: CGRect(origin: .zero, size: newSize))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
-    func hashImage(image: UIImage) -> String? {
-        guard let imageData = image.jpegData(compressionQuality: 1.0) else {
-            return nil
-        }
-        
-        let hash = SHA256.hash(data: imageData)
-        let hashString = hash.compactMap { String(format: "%02x", $0) }.joined()
-        
-        return hashString
-    }
-
-    func compareImages(image1: UIImage, image2: UIImage) -> Bool {
-        if let hash1 = hashImage(image: image1), let hash2 = hashImage(image: image2) {
-            return hash1 == hash2
-        }
-        return false
-    }
-    
-    func fetchAllPhotos(completion: @escaping ([String], [UIImage]) -> Void) {
-        // Tạo một mảng để lưu trữ tất cả các ảnh
-        var arr: [String] = []
-        var images: [UIImage] = []
-
-        // Tạo một đối tượng PHImageManager để truy cập ảnh
-        let imageManager = PHImageManager.default()
-
-        // Tạo một đối tượng PHFetchOptions để chỉ định các tùy chọn truy vấn
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-
-        // Thực hiện truy vấn để lấy tất cả các ảnh
-        let allPhotosResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-
-        // Lặp qua tất cả các ảnh và truy cập chúng
-        allPhotosResult.enumerateObjects { (asset, index, stop) in
-            let requestOptions = PHImageRequestOptions()
-            requestOptions.isSynchronous = true
-
-            // Lấy ảnh từ PHAsset
-            imageManager.requestImage(for: asset, targetSize: CGSize(width: 200, height: 200), contentMode: .aspectFill, options: requestOptions, resultHandler: { (image, info) in
-                if let image = image {
-                    // Thêm ảnh vào mảng allPhotos
-                    images.append(image)
-                    arr.append(self.hashImage(image: image)!)
-                    if images.count == allPhotosResult.count {
-                        completion(arr, images)
-                    }
-                }
-            })
-        }
-    }
-    
 }
--- Cleaner/TabBarViewController/CleanViewController.swift
+++ Cleaner/TabBarViewController/CleanViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Photos
+import CryptoKit
 
 class CleanViewController: UIViewController {
 
@@ -24,6 +25,17 @@ class CleanViewController: UIViewController {
     var similarSize = ""
     var duplicatedCount = 0
     var duplicatedSize = ""
+    var resizedImage: UIImage?
+    var grayImage: UIImage?
+    var finalImage: UIImage?
+    var grayValue: Double = 0
+    var comparisonResult: [[Int]] = []
+    var comparisonResults: [[[Int]]] = []
+    var images: [UIImage] = []
+    var similarTotalSize: Int = 0
+    var hashArr: [String] = []
+    public static var similarDataTable: [[UIImage]] = []
+    public static var duplicatedDataTable: [[UIImage]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +44,62 @@ class CleanViewController: UIViewController {
         duplicatedPhotosBtn.layer.cornerRadius = 12
         clearBtn.layer.cornerRadius = 25
         
+        var temp: [UIImage] = []
+        var value = 0
         DispatchQueue.main.async {
             self.fetchScreenshotsAlbum()
             self.countAndSizeScreenshotsLb.text = "\(self.screenshotsCount) photo(s) | \(self.screenshotsSize)"
+            
+            self.fetchAllPhotos { comparisonResults, hashArr, images in
+                self.comparisonResults = comparisonResults
+                self.images = images
+                self.hashArr = hashArr
+                for i in 0 ..< self.comparisonResults.count - 1 {
+                    value = self.compareArrays(array1: self.comparisonResults[i], array2: self.comparisonResults[i + 1])
+                    if value > 0 && value <= 8 {
+                        print("giong \(i) \(i + 1) \(value)")
+                        temp = [images[i], images[i + 1]]
+                        CleanViewController.similarDataTable.append(temp)
+                        self.similarTotalSize += self.fileSize(of: images[i + 1])
+                        self.similarCount += 1
+                    } else {
+                        print("khac \(i) \(i + 1)")
+                    }
+                }
+                
+                var result: [[UIImage]] = []
+                var currentIndex = 0
+                var addedElement: [String] = []
+                
+                while currentIndex < self.hashArr.count {
+                    let currentString = self.hashArr[currentIndex]
+                    var currentGroup: [String] = [currentString]
+                    
+                    let currentImage = self.images[currentIndex]
+                    var currentImageGroup: [UIImage] = [currentImage]
+                    
+                    var nextIndex = currentIndex + 1
+                    while nextIndex < self.hashArr.count {
+                        if self.hashArr[nextIndex] == currentString && !addedElement.contains(currentString) {
+                            currentGroup.append(self.hashArr[nextIndex])
+                            currentImageGroup.append(self.images[nextIndex])
+                        }
+                        nextIndex += 1
+                    }
+                    if currentGroup.count >= 2 {
+                        result.append(currentImageGroup)
+                        addedElement.append(currentString)
+                    }
+                    
+                    currentIndex += 1
+                    if currentIndex == self.hashArr.count {
+                        CleanViewController.duplicatedDataTable = result
+                        print(result)
+                    }
+                }
+            }
+            self.similarSize = self.formatSize(Int64(self.similarTotalSize))
+            self.countAndSizeSimilarLb.text = "\(self.similarCount) photo(s) | \(self.similarSize)"
         }
     }
     
@@ -48,6 +113,21 @@ class CleanViewController: UIViewController {
     
     @IBAction func duplicatedBtnTapped(_ sender: UIButton) {
         self.performSegue(withIdentifier: "duplicatedSegue", sender: self)
+    }
+    
+    func fileSize(of image: UIImage?) -> Int {
+        guard let imageData = image?.jpegData(compressionQuality: 1.0) else {
+            return 0
+        }
+        
+        return imageData.count
+    }
+    
+    func formatSize(_ size: Int64) -> String {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = [.useGB, .useMB, .useKB]
+        formatter.countStyle = .file
+        return formatter.string(fromByteCount: size)
     }
     
     func fetchScreenshotsAlbum() {
@@ -100,11 +180,279 @@ class CleanViewController: UIViewController {
         return (assetCount, totalSize)
     }
     
-    func formatSize(_ size: Int64) -> String {
-        let formatter = ByteCountFormatter()
-        formatter.allowedUnits = [.useGB, .useMB, .useKB]
-        formatter.countStyle = .file
-        return formatter.string(fromByteCount: size)
+    func resizeImage(image: UIImage, newSize: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: CGRect(origin: .zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
+    func resizeTo8x8(image: UIImage) -> UIImage? {
+        let newSize = CGSize(width: 8, height: 8)
+        return resizeImage(image: image, newSize: newSize)
+    }
+    
+    func convertToGrayScale(image: UIImage) -> UIImage? {
+        guard let cgImage = image.cgImage else {
+            return nil
+        }
+        
+        let width = cgImage.width
+        let height = cgImage.height
+        
+        let bytesPerPixel = 4 // RGBA
+        let bytesPerRow = width * bytesPerPixel
+        let bitsPerComponent = 8
+        
+        let bitmapInfo = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
+        
+        guard let context = CGContext(data: nil,
+                                      width: width,
+                                      height: height,
+                                      bitsPerComponent: bitsPerComponent,
+                                      bytesPerRow: bytesPerRow,
+                                      space: CGColorSpaceCreateDeviceRGB(),
+                                      bitmapInfo: bitmapInfo) else {
+            return nil
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        context.draw(cgImage, in: rect)
+        
+        guard let data = context.data?.assumingMemoryBound(to: UInt8.self) else {
+            return nil
+        }
+        
+        let grayColorSpace = CGColorSpaceCreateDeviceGray()
+        guard let grayContext = CGContext(data: nil,
+                                          width: width,
+                                          height: height,
+                                          bitsPerComponent: bitsPerComponent,
+                                          bytesPerRow: width,
+                                          space: grayColorSpace,
+                                          bitmapInfo: 0),
+              let grayData = grayContext.data?.assumingMemoryBound(to: UInt8.self) else {
+            return nil
+        }
+        
+        for i in 0..<width * height {
+            let r = Double(data[i * bytesPerPixel])
+            let g = Double(data[i * bytesPerPixel + 1])
+            let b = Double(data[i * bytesPerPixel + 2])
+            
+            let grayValue = UInt8((r + g + b) / 3.0)
+            grayData[i] = grayValue
+        }
+        
+        guard let grayImage = grayContext.makeImage() else {
+            return nil
+        }
+        
+        return UIImage(cgImage: grayImage)
+    }
+
+
+    func convertTo64Levels(image: UIImage) -> UIImage? {
+        guard let cgImage = image.cgImage else {
+            return nil
+        }
+
+        let width = cgImage.width
+        let height = cgImage.height
+
+        let bytesPerPixel = 1 // Grayscale
+        let bytesPerRow = width * bytesPerPixel
+        let bitsPerComponent = 8
+
+        let bitmapInfo = CGBitmapInfo.byteOrderDefault.rawValue
+
+        guard let context = CGContext(data: nil,
+                                      width: width,
+                                      height: height,
+                                      bitsPerComponent: bitsPerComponent,
+                                      bytesPerRow: bytesPerRow,
+                                      space: CGColorSpaceCreateDeviceGray(),
+                                      bitmapInfo: bitmapInfo) else {
+            return nil
+        }
+
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+
+        context.draw(cgImage, in: rect)
+
+        guard let data = context.data else {
+            return nil
+        }
+
+        let pixelBuffer = data.bindMemory(to: UInt8.self, capacity: width * height)
+
+        for i in 0..<width * height {
+            let pixelValue = pixelBuffer[i]
+            let newPixelValue = UInt8((Double(pixelValue) / 255.0) * 63.0)
+            pixelBuffer[i] = newPixelValue
+        }
+
+        guard let newGrayImage = context.makeImage() else {
+            return nil
+        }
+
+        return UIImage(cgImage: newGrayImage)
+    }
+    
+    func averageGrayValue(image: UIImage) -> Double? {
+        guard let cgImage = image.cgImage else {
+            return nil
+        }
+        
+        let width = cgImage.width
+        let height = cgImage.height
+        
+        guard let data = cgImage.dataProvider?.data else {
+            return nil
+        }
+        
+        let pointer = CFDataGetBytePtr(data)
+        
+        var totalGrayValue: Double = 0.0
+        
+        for y in 0..<height {
+            for x in 0..<width {
+                let pixelInfo = (width * y + x) * 1 // Grayscale
+                let grayValue = Double(pointer![pixelInfo])
+                totalGrayValue += grayValue
+            }
+        }
+        
+        let pixelCount = width * height
+        let averageGrayValue = totalGrayValue / Double(pixelCount)
+        
+        return averageGrayValue
+    }
+    
+    func compareGrayValues(image: UIImage, averageValue: Double) -> [[Int]]? {
+        guard let cgImage = image.cgImage else {
+            return nil
+        }
+        
+        let width = cgImage.width
+        let height = cgImage.height
+        
+        guard let data = cgImage.dataProvider?.data else {
+            return nil
+        }
+        
+        let pointer = CFDataGetBytePtr(data)
+        
+        var result: [[Int]] = Array(repeating: Array(repeating: 0, count: width), count: height)
+        
+        for y in 0..<height {
+            for x in 0..<width {
+                let pixelInfo = (width * y + x) * 1 // Grayscale
+                let grayValue = Double(pointer![pixelInfo])
+                
+                if grayValue >= averageValue {
+                    result[y][x] = 1
+                } else {
+                    result[y][x] = 0
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    func combineComparisonResults(results: [[Int]]) -> UInt64 {
+        var combinedResult: UInt64 = 0
+        
+        for row in results {
+            for bit in row {
+                combinedResult = (combinedResult << 1) | UInt64(bit)
+            }
+        }
+        
+        return combinedResult
+    }
+    
+    func compareArrays(array1: [[Int]], array2: [[Int]]) -> Int {
+        guard array1.count == array2.count && !array1.isEmpty && !array2.isEmpty else {
+            return 0
+        }
+        
+        let rowCount = array1.count
+        let columnCount = array1[0].count
+        
+        var count = 0
+        
+        for i in 0..<rowCount {
+            for j in 0..<columnCount {
+                if array1[i][j] != array2[i][j] {
+                    count += 1
+                }
+            }
+        }
+        
+        return count
+    }
+    
+    func hashImage(image: UIImage) -> String? {
+        guard let imageData = image.jpegData(compressionQuality: 1.0) else {
+            return nil
+        }
+        
+        let hash = SHA256.hash(data: imageData)
+        let hashString = hash.compactMap { String(format: "%02x", $0) }.joined()
+        
+        return hashString
+    }
+
+    func compareImages(image1: UIImage, image2: UIImage) -> Bool {
+        if let hash1 = hashImage(image: image1), let hash2 = hashImage(image: image2) {
+            return hash1 == hash2
+        }
+        return false
+    }
+    
+    func fetchAllPhotos(completion: @escaping ([[[Int]]], [String], [UIImage]) -> Void) {
+        // Tạo một mảng để lưu trữ tất cả các ảnh
+        var arr: [[[Int]]] = []
+        var hashArr: [String] = []
+        var images: [UIImage] = []
+
+        // Tạo một đối tượng PHImageManager để truy cập ảnh
+        let imageManager = PHImageManager.default()
+
+        // Tạo một đối tượng PHFetchOptions để chỉ định các tùy chọn truy vấn
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+
+        // Thực hiện truy vấn để lấy tất cả các ảnh
+        let allPhotosResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+
+        // Lặp qua tất cả các ảnh và truy cập chúng
+        allPhotosResult.enumerateObjects { (asset, index, stop) in
+            let requestOptions = PHImageRequestOptions()
+            requestOptions.isSynchronous = true
+
+            // Lấy ảnh từ PHAsset
+            imageManager.requestImage(for: asset, targetSize: CGSize(width: 200, height: 200), contentMode: .aspectFill, options: requestOptions, resultHandler: { (image, info) in
+                if let image = image {
+                    // Thêm ảnh vào mảng allPhotos
+                    images.append(image)
+                    hashArr.append(self.hashImage(image: image)!)
+                    self.resizedImage = self.resizeTo8x8(image: self.resizeImage(image: image, newSize: CGSize(width: 100, height: 100))!)
+                    self.grayImage = self.convertToGrayScale(image: self.resizedImage!)
+                    self.finalImage = self.convertTo64Levels(image: self.grayImage!)
+                    self.grayValue = self.averageGrayValue(image: self.finalImage!)!
+                    self.comparisonResult = self.compareGrayValues(image: self.finalImage!, averageValue: self.grayValue)!
+                    arr.append(self.comparisonResult)
+                    if arr.count == allPhotosResult.count {
+                        completion(arr, hashArr, images)
+                    }
+                }
+            })
+        }
     }
 }
 
