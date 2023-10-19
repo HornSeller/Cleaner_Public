3f5f92f8565a1217c8645ff4e7f494a6c00bc928
Duc Nguyen <108277804+HornSeller@users.noreply.github.com>
2023-10-19 15:14:17 +0700

    commit

--- Cleaner/SettingViewController.swift
+++ Cleaner/SettingViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 import PasscodeKit
+import MessageUI
+import StoreKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var switchBtn: UISwitch!
     @IBOutlet weak var goPremiumBtn: UIButton!
@@ -36,6 +38,27 @@ class SettingViewController: UIViewController {
         }
     }
     
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+
+        switch result {
+        case .cancelled:
+            // Xử lý khi người dùng hủy gửi email
+            break
+        case .saved:
+            // Xử lý khi email được lưu như phiên bản nháp
+            break
+        case .sent:
+            // Xử lý khi email được gửi thành công
+            break
+        case .failed:
+            // Xử lý khi gửi email thất bại
+            break
+        @unknown default:
+            break
+        }
+    }
+    
     @IBAction func backBtnTapped(_ sender: UIBarButtonItem) {
         self.navigationController?.popViewController(animated: true)
     }
@@ -58,7 +81,40 @@ class SettingViewController: UIViewController {
             PasscodeKit.changePasscode(self)
         }
     }
-        
+    
+    @IBAction func feedbackBtnTapped(_ sender: UIButton) {
+        if MFMailComposeViewController.canSendMail() {
+            let composeVC = MFMailComposeViewController()
+            composeVC.mailComposeDelegate = self
+            composeVC.setToRecipients(["youraddress@example.com"])
+            composeVC.setSubject("Feedback your app!")
+            composeVC.setMessageBody("Dear AppChannel team, \n\n", isHTML: false)
+            self.present(composeVC, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "Error", message: "Please try again later", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
+    
+    @IBAction func rateBtnTapped(_ sender: UIButton) {
+        SKStoreReviewController.requestReview()
+    }
+    
+    @IBAction func shareBtnTapped(_ sender: UIButton) {
+        if let name = URL(string: "https://apps.apple.com/vn/app/asphalt-9-legends/id1491129197?mt=12"), !name.absoluteString.isEmpty {
+          let objectsToShare = [name]
+          let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+          self.present(activityVC, animated: true, completion: nil)
+        } else {
+          // show alert for not available
+            let alert = UIAlertController(title: "Error", message: "Please try again later", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
+    
     static func makeSelf() -> SettingViewController {
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let rootViewController: SettingViewController = storyboard.instantiateViewController(withIdentifier: "SettingViewController") as! SettingViewController
--- Cleaner/SubHomeViewController/SpeedTestViewController.swift
+++ Cleaner/SubHomeViewController/SpeedTestViewController.swift
@@ -27,6 +27,12 @@ class SpeedTestViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [
+                    NSAttributedString.Key.foregroundColor: UIColor.white,
+                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)
+        ]
+        
         circularProgressFrame = CGRect(x: (self.view.frame.width - circularProgressWidth) / 2, y: self.view.frame.height * 0.255, width: circularProgressWidth, height: circularProgressWidth)
         circularProgress1 = KDCircularProgress(frame: circularProgressFrame)
         circularProgress2 = KDCircularProgress(frame: circularProgressFrame)
